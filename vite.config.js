6cf5645e1d24a666fc2a748b1cfbb3e4c43a51d8
amirali <amiralimirabdolah@gmail.com>
2025-06-07 17:19:02 +0330

    Fixed path in vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // base:"https://amirali-mirabdolah.github.io/emy-style/"
+  base: "/emy-style/",
   plugins: [
     react(),
     tailwindcss(),
